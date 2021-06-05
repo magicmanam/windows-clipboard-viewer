a93e774a94800e8afe66b20c2107f8f4a00ac8bf
Andrei Muski <magicmanam@gmail.com>
2021-06-05 11:18:44 +0300

    Adds initial implementation

---
+++ magicmanam.Windows.ClipboardViewer/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("magicmanam.Windows.ClipboardViewer")]
+[assembly: AssemblyDescription("Easy to Windows platform ClipboardViewer wrapper")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("magicmanam")]
+[assembly: AssemblyProduct("magicmanam.Windows.ClipboardViewer")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyTrademark("magicmanam")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3dd7b207-5743-457f-a530-7669d0d2ced7")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
